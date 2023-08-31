8ad54a0b14d5c8163b55d0c4f9b1cd63a13783e7
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-08-31 21:39:16 +0530

    ArrayList As Deque Java Example

---
+++ queue-example/src/ArrayDequeAsStackJavaExample.java
@@ -0,0 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ArrayDequeAsStackJavaExample {
+    public static void main(String[] args) throws InterruptedException {
+
+        Deque<Integer> integerArrayDeque = new ArrayDeque<>();
+
+        integerArrayDeque.push(10);
+
+
+    }
+}
