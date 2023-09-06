bc5e3d4ab67ebe4a6e4f11edeed948b369ef0a75
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-09-06 21:47:01 +0530

    Linked List Example

--- list-example/src/LinkedListExample.java
+++ list-example/src/LinkedListExample.java
@@ -1,2 +1,20 @@
+import org.w3c.dom.Node;
+
 public class LinkedListExample {
+    Node head;
+
+    LinkedListExample(){
+        head = null;
+
+    }
+    static class Node{
+        int data;
+        Node next;
+
+         Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
 }
