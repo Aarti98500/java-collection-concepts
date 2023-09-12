e86a090967ab43298a241cb3aa9504b0c59204b7
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-09-12 22:43:53 +0530

    Tree map java example

---
+++ map-examples/src/TreeMapJavaExample.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapJavaExample {
+    public static void main(String[] args) {
+        Map<String, Long> myPhoneBook = new TreeMap<>();
+
+        myPhoneBook.put("Aarti", 1010101010L);
+        myPhoneBook.put("Komal", 2020202020L);
+        myPhoneBook.put("Karan", 3030303030L);
+
+    }
+}
