46d34d403437cd9a4237f3619370ccab317fadd3
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-09-17 20:22:29 +0530

    Hash map java example

--- map-examples/src/HashMapJavaExample.java
+++ map-examples/src/HashMapJavaExample.java
@@ -9,7 +9,7 @@ public class HashMapJavaExample {
 
         myPhoneBook.put("Aarti", 1010101010L);
         myPhoneBook.put("Komal", 2020202020L);
-        myPhoneBook.put("Karan", 3030303030L);
+        myPhoneBook.put("Komal", 3030303030L);
         myPhoneBook.put("Vikram", 4040404040L);
 
         myPhoneBook.put(null, null);
@@ -24,9 +24,12 @@ public class HashMapJavaExample {
         for(String key : myPhoneBook.keySet()){
             System.out.println("key = " + key);
         }
-        System.out.println("--------values----------");
-        for (Long value: myPhoneBook.values());{
-            System.out.println("v = " + value);
+        System.out.println("--------values--------");
+
+        for (Long value : myPhoneBook.values()){
+            System.out.println("v = "+ value);
+
+
         }
         System.out.println("----------Entries---------");
         for (Map.Entry<String, Long> entry : myPhoneBook.entrySet()){
