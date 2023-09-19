dbb467e747afcf005819e53e0b9843d7703a49f0
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-09-19 19:52:13 +0530

    Linked Hash set example

--- set-example/src/LinkedHashSetExample.java
+++ set-example/src/LinkedHashSetExample.java
@@ -1,2 +1,32 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+//insertion order is maintained.
 public class LinkedHashSetExample {
+    public static void main(String[] args) {
+        Set<String> fruitNames = new LinkedHashSet<>();
+
+        fruitNames.add("Apple");
+        fruitNames.add("Mango");
+        fruitNames.add("Orange");
+        fruitNames.add("Grapes");
+
+        System.out.println("----------------------------");
+        System.out.println("isEmpty set ? " + fruitNames.isEmpty());
+        System.out.println("--------------------------");
+        System.out.println("size : " + fruitNames.size());
+        System.out.println("---------------------------");
+        for (String fruitName :fruitNames){
+            System.out.println("Name : " + fruitName);
+        }
+        System.out.println("---------------------");
+        fruitNames.remove("Apple");
+
+        for (String fruitName : fruitNames){
+            System.out.println("Name : " + fruitName );
+        }
+        System.out.println("---------------------");
+        System.out.println("size : " + fruitNames.size());
+        System.out.println("-----------------------");
+    }
 }
