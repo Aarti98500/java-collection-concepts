56df6dd4659df392eaccfe723cb2cfd3df8dc035
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-09-29 12:42:11 +0530

    vector example

---
+++ stack-example/src/VectorExample.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+
+        vector.add("Aarti");
+        vector.add("Mauli");
+
+        vector.remove(0);
+
+    }
+}
